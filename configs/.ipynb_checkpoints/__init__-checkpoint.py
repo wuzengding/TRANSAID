2c4c487e5d7429968b11779d8673ab0823cdda95
wuzengding <wuzengding@163.com>
2024-11-27 10:43:45 +0800

    _

--- configs/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,8 +0,0 @@
-# configs/__init__.py
-from .model_config import ModelConfig
-from .training_config import TrainingConfig
-
-__all__ = [
-    'ModelConfig',
-    'TrainingConfig'
-]
--- src/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from . import features
-from . import models
-from . import data
-
-__version__ = '1.0.0'
